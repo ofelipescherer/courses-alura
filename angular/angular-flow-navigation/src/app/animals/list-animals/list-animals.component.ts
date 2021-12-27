77f0a3222607a47dc3bdc0745416d67fd28119c1
Felipe Scherer <felipescherer25@gmail.com>
2021-12-27 11:56:09 -0300

    [angular-flow-navigation] feat: animal component

--- angular/angular-flow-navigation/src/app/animals/list-animals/list-animals.component.ts
+++ angular/angular-flow-navigation/src/app/animals/list-animals/list-animals.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+const API = 'http://localhost:3000';
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
-  styleUrls: ['./list-animals.component.css']
+  styleUrls: ['./list-animals.component.css'],
 })
 export class ListAnimalsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private originalUrl = '';
+  @Input() description = '';
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.originalUrl = url;
+    } else {
+      this.originalUrl = `${API}/img/${url}`;
+    }
   }
 
+  get url(): string {
+    return this.originalUrl;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
