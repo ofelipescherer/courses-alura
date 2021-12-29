32e4fca6e99d1e310b14c73eed4279e4698c799a
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 16:10:41 -0300

    [angular-flow-navigation] feat: animal template

--- angular/angular-flow-navigation/src/app/animals/animals-routing.module.ts
+++ angular/angular-flow-navigation/src/app/animals/animals-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListAnimalsComponent,
+  },
+  {
+    path: ':animalId',
+    component: AnimalDetailComponent,
   },
 ];
 
