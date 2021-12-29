728595af34bdff650347d02462e3d483747b51d3
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 19:10:11 -0300

    [angular-flow-navigation] feat: comment template

--- angular/angular-flow-navigation/src/app/animals/animal-detail/comments/comments.component.ts
+++ angular/angular-flow-navigation/src/app/animals/animal-detail/comments/comments.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comments } from './comments';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+  styleUrls: ['./comments.component.css'],
 })
 export class CommentsComponent implements OnInit {
+  comments$!: Observable<Comments>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- angular/angular-flow-navigation/src/app/animals/animals.module.ts
+++ angular/angular-flow-navigation/src/app/animals/animals.module.ts
@@ -4,13 +4,25 @@ import { CommonModule } from '@angular/common';
 import { AnimalsRoutingModule } from './animals-routing.module';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 import { AnimalComponent } from './animal/animal.component';
-import { CardComponent } from '../components/card/card.component';
 import { CardModule } from '../components/card/card.module';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { CommentsComponent } from './animal-detail/comments/comments.component';
+import { MessageModule } from '../components/message/message.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ListAnimalsComponent, AnimalComponent, AnimalDetailComponent, CommentsComponent],
-  imports: [CommonModule, AnimalsRoutingModule, CardModule],
+  declarations: [
+    ListAnimalsComponent,
+    AnimalComponent,
+    AnimalDetailComponent,
+    CommentsComponent,
+  ],
+  imports: [
+    CommonModule,
+    AnimalsRoutingModule,
+    CardModule,
+    MessageModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AnimalsModule {}
