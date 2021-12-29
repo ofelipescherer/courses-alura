0bdc189a3a1eede15af6c94f56da38f67533215b
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 16:18:59 -0300

    [angular-flow-navigation] feat: activated route

--- angular/angular-flow-navigation/src/app/animals/animal-detail/animal-detail.component.ts
+++ angular/angular-flow-navigation/src/app/animals/animal-detail/animal-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Animal } from '../animals';
+import { AnimalsService } from '../animals.service';
 
 @Component({
   selector: 'app-animal-detail',
   templateUrl: './animal-detail.component.html',
-  styleUrls: ['./animal-detail.component.css']
+  styleUrls: ['./animal-detail.component.css'],
 })
 export class AnimalDetailComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(
+    private animalService: AnimalsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animalService.searchId(this.animalId);
   }
-
 }
--- angular/angular-flow-navigation/src/app/animals/animals.service.ts
+++ angular/angular-flow-navigation/src/app/animals/animals.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../authenticator/token.service';
-import { Animals } from './animals';
+import { Animal, Animals } from './animals';
 
 const API = environment.apiURL;
 
@@ -17,5 +17,11 @@ export class AnimalsService {
     const token = this.tokenService.returnToken();
     const headers = new HttpHeaders().append('x-access-token', token);
     return this.http.get<Animals>(`${API}/${userName}/photos`, { headers });
+  }
+
+  searchId(id: number): Observable<Animal> {
+    const token = this.tokenService.returnToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animal>(`${API}/photos/${id}`, { headers });
   }
 }
