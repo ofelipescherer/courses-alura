d9a028ad3a3932a5fd132a83fcb7d4127bf5708b
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 16:35:44 -0300

    [angular-flow-navigation] feat: methods like and delete

--- angular/angular-flow-navigation/src/app/animals/animals.service.ts
+++ angular/angular-flow-navigation/src/app/animals/animals.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mapTo } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../authenticator/token.service';
 import { Animal, Animals } from './animals';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +21,20 @@ export class AnimalsService {
 
   searchId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  deleteAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  likeAnimal(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
