088e219cd990d4c6f4eae97115cf1d537173bf62
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 16:22:35 -0300

    [angular-flow-navigation] feat: interceptor

---
+++ angular/angular-flow-navigation/src/app/authenticator/authenticator.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
+
+@Injectable()
+export class AuthenticatorInterceptor implements HttpInterceptor {
+  constructor(private tokenService: TokenService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (this.tokenService.hasToken()) {
+      const token = this.tokenService.returnToken();
+      const headers = new HttpHeaders().append('x-access-token', token);
+      request = request.clone({ headers });
+    }
+
+    return next.handle(request);
+  }
+}
