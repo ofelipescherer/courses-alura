a2628fe78e715a96d8d15300688fcd858fdcce5c
Felipe Scherer <felipescherer25@gmail.com>
2021-12-29 10:33:55 -0300

    [angular-flow-navigation] feat: card-animal

---
+++ angular/angular-flow-navigation/src/app/animals/animal/animal.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const API = environment.apiURL;
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css'],
+})
+export class AnimalComponent implements OnInit {
+  private originalUrl = '';
+  @Input() description = '';
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.originalUrl = url;
+    } else {
+      this.originalUrl = `${API}/img/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.originalUrl;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- angular/angular-flow-navigation/src/app/animals/list-animals/list-animals.component.ts
+++ angular/angular-flow-navigation/src/app/animals/list-animals/list-animals.component.ts
@@ -1,27 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { Animals } from '../animals';
 
-const API = environment.apiURL;
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
   styleUrls: ['./list-animals.component.css'],
 })
 export class ListAnimalsComponent implements OnInit {
-  private originalUrl = '';
-  @Input() description = '';
-  @Input() set url(url: string) {
-    if (url.startsWith('data')) {
-      this.originalUrl = url;
-    } else {
-      this.originalUrl = `${API}/img/${url}`;
-    }
-  }
-
-  get url(): string {
-    return this.originalUrl;
-  }
-
+  @Input() animals!: Animals;
   constructor() {}
 
   ngOnInit(): void {}
