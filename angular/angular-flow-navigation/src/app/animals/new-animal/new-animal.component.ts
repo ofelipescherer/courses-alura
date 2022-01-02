a9755aa67c946ba2821f2b75eb34fedb5677c67c
Felipe Scherer <felipescherer25@gmail.com>
2022-01-01 21:13:33 -0300

    [angular-flow-navigation] feat: animal service

--- angular/angular-flow-navigation/src/app/animals/new-animal/new-animal.component.ts
+++ angular/angular-flow-navigation/src/app/animals/new-animal/new-animal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { AnimalsService } from '../animals.service';
 
 @Component({
   selector: 'app-new-animal',
@@ -12,11 +15,36 @@ export class NewAnimalComponent implements OnInit {
   preview!: string;
   percent = 0;
 
-  constructor() {}
+  constructor(
+    private animalsService: AnimalsService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.animalForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
+  }
 
-  upload() {}
+  upload() {
+    const allowComments = this.animalForm.get('allowComments')?.value ?? false;
+    const description = this.animalForm.get('description')?.value ?? '';
 
-  saveFile(file: any) {}
+    this.animalsService
+      .upload(description, allowComments, this.file)
+      .pipe(finalize(() => this.router.navigate(['animals'])));
+  }
+
+  saveFile(file: any): void {
+    const [savedFile] = file?.files;
+    this.file = savedFile;
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.preview = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
